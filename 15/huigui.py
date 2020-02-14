2ff0eb23668ebd29c8b4503247fb76b24eb36732
zwdnet <zwdnet@163.com>
2020-02-14 14:45:38 +0800

    增加模型检验，再上传到服务器。

--- 15/huigui.py
+++ 15/huigui.py
@@ -11,20 +11,20 @@ import pandas as pd
 if __name__ == "__main__":
 	x = [3.4, 1.8, 4.6, 2.3, 3.1, 5.5, 0.7, 3, 2.6, 4.3, 2.1, 1.1, 6.1, 4.8, 3.8]
 	y = [26.2, 17.8, 31.3, 23.1, 27.5, 36, 14.1, 22.3, 19.6, 31.3, 24, 17.3, 43.2, 36.4, 26.1]
-	x = np.array(x)
-	y = np.array(y)
+	# x = np.array(x)
+	# y = np.array(y)
 	print(len(x), len(y))
 	plt.scatter(x, y)
 	plt.savefig("scatter.png")
 	plt.close()
 	# 建立线性回归模型
-	df = pd.DataFrame()
-	df["X"] = x
-	df["Y"] = y
-	print(df)
+	# df = pd.DataFrame()
+	# df["X"] = x
+	# df["Y"] = y
+	# print(df)
 	regr = linear_model.LinearRegression()
 	# 拟合
-	regr.fit(x.reshape(-1,1), y)
+	regr.fit(x, y)
 	# 得到回归参数的二乘法估计
 	a, b = regr.coef_, regr.intercept_
 	print(a, b)
@@ -33,4 +33,4 @@ if __name__ == "__main__":
 	plt.scatter(x, y)
 	plt.plot(x, yp)
 	plt.savefig("fit.png")
-	
+	regr.score(x, y)
