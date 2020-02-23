918a7de9ec961f055d0e523f1253d14efa3f9aba
zwdnet <zwdnet@163.com>
2020-02-23 17:11:43 +0800

    传到服务器，进行逻辑回归。

--- 20/logist.py
+++ 20/logist.py
@@ -32,5 +32,23 @@ if __name__ == "__main__":
 	
 	# 逻辑回归模型
 	lr = LogisticRegression(C = 1e5)
+	lr.fit(X, Y)
 	
+	h = 0.02
+	x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
+	y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+	xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+	
+	Z = Z.reshape(xx.shape)
+	plt.figure(1, figsize = (8, 6))
+	plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+	plt.savefig("result.png")
+	plt.close()
+	
+	plt.scatter(X[:50, 0], X[:50, 1], color = "red", marker = "o", label = "1")
+	plt.scatter(X[50:100, 0], X[50:100, 1], color = "blue", marker = "x", label = "2")
+	plt.scatter(X[100:, 0], X[100:, 1], color = "green", marker = "+", label = "3")
+	plt.legend(loc = 2)
+	plt.savefig("scatter2.png")
+	plt.close()
 	
