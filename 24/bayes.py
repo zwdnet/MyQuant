49851c424915be3a34ae89afc9e6dbd2b4e65d14
zwdnet <zwdnet@163.com>
2020-02-25 19:58:02 +0800

    朴素贝叶斯算法测试，传到服务器。

---
+++ 24/bayes.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+# 朴素贝叶斯的例子
+
+
+import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
+
+
+# 数据转换
+def transform(data):
+    data.loc[data.Age == "青少年", "Age"] = 1
+    data.loc[data.Age == "中年", "Age"] = 2
+    data.loc[data.Age == "老年", "Age"] = 3
+    data.loc[data.Income == "高", "Income"] = 1
+    data.loc[data.Income == "中", "Income"] = 2
+    data.loc[data.Income == "低", "Income"] = 3
+    data.loc[data.Alone == "是", "Alone"] = 1
+    data.loc[data.Alone == "否", "Alone"] = 2
+    data.loc[data.Credit == "良好", "Credit"] = 1
+    data.loc[data.Credit == "一般", "Credit"] = 2
+    data.loc[data.Buy == "是", "Buy"] = 1
+    data.loc[data.Buy == "否", "Buy"] = 2
+    return data
+
+
+if __name__ == "__main__":
+    data = pd.read_csv("data.csv")
+    print(data)
+    data = transform(data)
+    print(data)
+    
+    test = pd.Series({"Age":3, "Income":3, "Alone":2, "Credit":2})
+    print(test)
+    
+    # 进行朴素贝叶斯分类模型训练
+    clf = MultinomialNB(alpha = 2.0)
+    clf.fit(data.values, test.data)
+    print(clf.class_log_prior_)
+    # 用模型预测
+    print(clf.predict(test.values))
+    print(clf.predict_proba(test.values))
+    
