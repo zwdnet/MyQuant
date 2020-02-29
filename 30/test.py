e5dccd9964914cda9c52ddda0c6fd58174a24a1e
zwdnet <zwdnet@163.com>
2020-02-29 20:57:47 +0800

    决策树分类算法，传到服务器。

--- 30/test.py
+++ 30/test.py
@@ -3,9 +3,13 @@
 
 
 from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
 
 
 if __name__ == "__main__":
+    # KNN算法
     X = [[0], [1], [2], [3]]
     y = [0, 0, 1, 1]
     
@@ -13,4 +17,11 @@ if __name__ == "__main__":
     neigh.fit(X, y)
     print(neigh.predict([[1.1]]))
     
+    # 决策树算法
+    # 创建一颗基于基尼系数的决策树
+    clf = DecisionTreeClassifier()
+    data = load_iris()
+    # 交叉验证
+    score = cross_val_score(clf, data.data, data.target, cv = 10)
+    print(score)
     
