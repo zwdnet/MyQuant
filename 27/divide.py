548de3c29fc2d8c280e16cc2ff317d08c0d78abb
zwdnet <zwdnet@163.com>
2020-02-29 11:03:36 +0800

    用kmeans算法进行图像分割，中间过程，传到服务器运行看看。

---
+++ 27/divide.py
@@ -0,0 +1,32 @@
+# coding:utf-8
+"""k-means图像分割实例"""
+
+
+import numpy as np
+import PIL.Image as image
+from sklearn.cluster import KMeans
+
+
+# 加载图片
+def loadData(filePath):
+    f = open(filePath, "rb")
+    data = []
+    img = image.open(f)
+    m,n = img.size
+    for i in range(m):
+        for j in range(n):
+            x, y, z = img.getpixel((i, j))
+            data.append([x/256.0, y/256.0, z/256.0])
+    f.close()
+    return np.mat(data), m, n
+
+
+if __name__ == "__main__":
+    imgData, row, col = loadData("test.jpg")
+    km = KMeans(n_clusters = 3)
+    # 聚类获取每个像素点颜色所属的类别
+    label = km.fit_predict(imgData)
+    print(label)
+    label = label.reshape([row, col])
+    print(label)
+    
