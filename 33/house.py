a83c769164aae841d6ec3ac44e75cf14143b1062
zwdnet <zwdnet@163.com>
2020-03-03 14:13:01 +0800

    结果有问题，改了传服务器试试。

--- 33/house.py
+++ 33/house.py
@@ -27,11 +27,12 @@ if __name__ == "__main__":
     # linear.fit(x.reshape(length, -1), y)
     poly_reg = PolynomialFeatures(degree = 2)
     X_poly = poly_reg.fit_transform(x.reshape([length, 1]))
+    print(X_poly)
     lin_reg2 = linear_model.LinearRegression()
     lin_reg2.fit(X_poly, y)
     # 获取回归参数
-    coef = lin_reg2.coef_
-    inter = lin_reg2.intercept_
+    coef = linear.coef_
+    inter = linear.intercept_
     print(coef, inter)
     # 可视化
     y = lin_reg2.predict(poly_reg.fit_transform(x.reshape([length, 1])))
