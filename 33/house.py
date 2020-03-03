24fdae74090e043e11cc493e22fed6f873ddad44
zwdnet <zwdnet@163.com>
2020-03-03 14:44:22 +0800

    再改

--- 33/house.py
+++ 33/house.py
@@ -10,34 +10,26 @@ from sklearn.preprocessing import PolynomialFeatures
 
 
 if __name__ == "__main__":
-    data = pd.read_csv("data.csv")
-    print(data.head())
-    x = np.array(data["area"])
-    y = np.array(data["price"])
-    minX = min(x)
-    maxX = max(x)
-    length = len(x)
-    X = np.arange(minX,maxX).reshape([-1,1]) 
-    print(x.shape, y.shape) 
-    print(x, x.reshape(-1, length))
-    plt.figure()
-    plt.scatter(x, y)
-    plt.savefig("scatter.png")
-    # 建模
-    # linear = linear_model.LinearRegression()
-    # linear.fit(x.reshape(length, -1), y)
+    # 读取数据集     
+    datasets_Y = [] 
+    fr = open('data.csv','r') 
+    lines = fr.readlines() for line in lines:
+        items = line.strip().split(',')
+        datasets_X.append(int(items[0]))
+        datasets_Y.append(int(items[1])
+    length = len(datasets_X)
+    datasets_X = np.array(datasets_X).reshape([length,1])
+    datasets_Y = np.array(datasets_Y)
+    minX = min(datasets_X)
+    maxX = max(datasets_X)
+    X = np.arange(minX,maxX).reshape([-1,1])
     poly_reg = PolynomialFeatures(degree = 2)
-    X_poly = poly_reg.fit_transform(x.reshape([length, 1]))
-    print(X_poly)
-    lin_reg2 = linear_model.LinearRegression()
-    lin_reg2.fit(X_poly, y)
-    # 获取回归参数
-    coef = lin_reg2.coef_
-    inter = lin_reg2.intercept_
-    print(coef, inter)
-    # 可视化
-    y = lin_reg2.predict(poly_reg.fit_transform(X_poly))
-    print(x.shape, y.shape)
-    plt.plot(x, y, color = "black")
+    X_poly = poly_reg.fit_transform(datasets_X)
+    lin_reg_2 = linear_model.LinearRegression()
+    lin_reg_2.fit(X_poly, datasets_Y)
+    # 图像中显示 
+    plt.scatter(datasets_X, datasets_Y, color = 'red')
+    plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+    plt.xlabel('Area')
+    plt.ylabel('Price')
     plt.savefig("result.png")
-    
