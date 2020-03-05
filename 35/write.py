c1f211a3934a6b7e3d90a085dd0105bbf7347e79
zwdnet <zwdnet@163.com>
2020-03-05 16:21:30 +0800

    神经网络识别手写，传到服务器。

--- 35/write.py
+++ 35/write.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 from os import listdir
-# from sklearn.neural_network import MLPclassifier
+from sklearn.neural_network import MLPclassifier
 
 
 # 加载训练数据
@@ -20,7 +20,7 @@ def img2vector(filename):
     
 # 加载训练数据集，并将标签转化为one-hot向量
 def readDataSet(Path):
-    fileList = listdir(path)
+    fileList = listdir(Path)
     numFiles = len(fileList)
     dataSet = np.zeros([numFiles, 1024], int)
     hwLabels = np.zeros([numFiles, 10])
@@ -32,9 +32,30 @@ def readDataSet(Path):
     return dataSet, hwLabels
 
 
-if __name__ == "__name__":
-    print("hello")
-    train_dataSet, train_hwLabels = readDataSet('./data/pendigits-orig.tes.Z')
-    print(len(train_dataSet))
-    print(train_hwLabels)
+if __name__ == "__main__":
+    # 读取数据，照https://github.com/JayZhuCoding/simple_neural_network/blob/master/Neural.py来
+    dataset = np.genfromtxt("./data/pendigits.tra", delimiter= ",")
+    m, n = dataset.shape
+    X = dataset[:,:n-1]
+    y = dataset[:,n-1]
+    X /= X.max()
+    
+    # 建立神经网络
+    clf = MLPclassifier(hidden_layer_sizes = (100,), activation = "logistic", solver = "adam", learning_rate_init = 0.0001, max_iter = 2000)
+    # 训练神经网络
+    clf.fit(X, y)
+    # 载入测试集
+    dataset_test = np.genfromtxt("./data/pendigits.tes", delimiter= ",")
+    m_test, n_test = dataset_test.shape
+    X_test = dataset_test[:,:n_test-1]
+    y_test = dataset[:,n_test-1]
+    X_test /= X_test.max()
+    # 测试
+    res = clf.predict(X_test)
+    error_num = 0
+    num = len(X_test)
+    for i in range(num):
+        if np.sum(res[i] == y_test[i]) < 10:
+            error_sum += 1
+    print(num, error_sum, error_sum/float(num))
     
