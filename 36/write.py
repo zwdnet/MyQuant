02189bbddd0181562fe273d08cd731be4ffb8350
zwdnet <zwdnet@163.com>
2020-03-05 17:04:02 +0800

    神经网络手写识别。

--- 36/write.py
+++ 36/write.py
@@ -2,60 +2,27 @@
 # 神经网络实现手写识别
 
 
-import numpy as np
-from os import listdir
 from sklearn.neural_network import MLPClassifier
-
-
-# 加载训练数据
-def img2vector(filename):
-    retMat = np.zeros([1024], int)
-    fr = open(filename)
-    lines = fr.readlines()
-    for i in range(32):
-        for j in range(32):
-            retMat[i*32+j] = lines[i][j]
-    return retMat
-    
-    
-# 加载训练数据集，并将标签转化为one-hot向量
-def readDataSet(Path):
-    fileList = listdir(Path)
-    numFiles = len(fileList)
-    dataSet = np.zeros([numFiles, 1024], int)
-    hwLabels = np.zeros([numFiles, 10])
-    for i in range(numFiles):
-        filePath = fileList[i]
-        digit = int(filePath.split("_")[0])
-        hwLabels[i][digit] = 1.0
-        dataSet[i] = img2vector(path + '/' + filePath)
-    return dataSet, hwLabels
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    # 读取数据，照https://github.com/JayZhuCoding/simple_neural_network/blob/master/Neural.py来
-    dataset = np.genfromtxt("./data/pendigits.tra", delimiter= ",")
-    m, n = dataset.shape
-    X = dataset[:,:n-1]
-    y = dataset[:,n-1]
-    X /= X.max()
+    # 加载数据
+    digits = load_digits()
+    x_data = digits.data
+    y_data = digits.target
+    print(x_data.shape)
+    print(y_data.shape)
     
-    # 建立神经网络
-    clf = MLPClassifier(hidden_layer_sizes = (100,), activation = "logistic", solver = "adam", learning_rate_init = 0.0001, max_iter = 2000)
-    # 训练神经网络
-    clf.fit(X, y)
-    # 载入测试集
-    dataset_test = np.genfromtxt("./data/pendigits.tes", delimiter= ",")
-    m_test, n_test = dataset_test.shape
-    X_test = dataset_test[:,:n_test-1]
-    y_test = dataset[:,n_test-1]
-    X_test /= X_test.max()
-    # 测试
-    res = clf.predict(X_test)
-    error_sum = 0
-    num = len(X_test)
-    for i in range(num):
-        if np.sum(res[i] == y_test[i]) < 10:
-            error_sum += 1
-    print(num, error_sum, error_sum/float(num))
+    # 划分训练测试集
+    x_train, x_test, y_train, y_test =  train_test_split(x_data, y_data)
+    # 训练
+    mlp = MLPClassifier(hidden_layer_sizes = (100, 50), max_iter = 500)
+    mlp.fit(x_train, y_train)
+    # 准确率评估
+    predictions = mlp.predict(x_test)
+    print(classification_report(y_test, predictions))
     
