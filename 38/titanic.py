15335184fa7260962466b27947734b1bc6ac6a3a
zwdnet <zwdnet@163.com>
2020-03-13 06:11:41 +0000

    在服务器上运行完毕，返回本地。

--- 38/titanic.py
+++ 38/titanic.py
@@ -252,6 +252,7 @@ if __name__ == "__main__":
     for df in dfs:
         for feature in cat_features:
             encoded_feat = OneHotEncoder().fit_transform(df[feature].values.reshape(-1,1)).toarray()
+            n = df[feature].nunique()
             cols = ["{}_{}".format(feature, n) for n in range(1, n+1)]
             encoded_df = pd.DataFrame(encoded_feat, columns = cols)
             encoded_df.index = df.index
