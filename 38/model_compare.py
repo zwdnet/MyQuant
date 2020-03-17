6fe854215f9407b2444f9bcaa716c83398fa6083
zwdnet <zwdnet@163.com>
2020-03-17 13:19:04 +0800

    模型比较，框架搭好，传到服务器。

---
+++ 38/model_compare.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+# 模型比较
+
+
+from sklearn.linear_model import LogisticRegression    #逻辑回归
+from sklearn.svm import SVC, LinearSVC                 #支持向量机
+from sklearn.ensemble import RandomForestClassifier    #随机森林
+from sklearn.neighbors import KNeighborsClassifier     #K最邻近算法
+from sklearn.naive_bayes import GaussianNB             #朴素贝叶斯
+from sklearn.linear_model import Perceptron            #感知机算法             
+from sklearn.linear_model import SGDClassifier         #梯度下降分类
+from sklearn.tree import DecisionTreeClassifier        #决策树算法
+from sklearn.model_selection import StratifiedKFold    #K折交叉切分
+from sklearn.model_selection import GridSearchCV       #网格搜索
+
+
+import tools
+
+
+# 尝试各种模型
+def model_compare(df_all):
+    # 划分数据
+    train_df, test_df = tools.divide_df(df_all)
+    X_train = train_df.drop("Survived", axis = 1)
+    Y_train = train_df["Survived"]
+    X_test = test_df
+    print(X_train.shape, Y_train.shape, X_test.shape)
+    
