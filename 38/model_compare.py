993244e557aba24f7c1f33b9fab53f8bde78ec41
zwdnet <zwdnet@163.com>
2020-03-17 14:25:32 +0800

    完成模型比较，上传到服务器。

--- 38/model_compare.py
+++ 38/model_compare.py
@@ -44,3 +44,44 @@ def model_compare(df_all):
     acc_svc = ModelTest(SVMModel, X_train, Y_train)
     print("支持向量机结果:{}".format(acc_svc))
     
+    # knn算法
+    knnModel = KNeighborsClassifier(n_neighbors = 3)
+    acc_knn = ModelTest(knnModel, X_train, Y_train)
+    print("knn结果:{}".format(acc_knn))
+    
+    # 朴素贝叶斯模型
+    BYSModel = GaussianNB()
+    acc_bys = ModelTest(BYSModel, X_train, Y_train)
+    print("朴素贝叶斯算法结果:{}".format(acc_bys))
+    
+    # 感知机算法
+    percModel = Perceptron()
+    acc_perc = ModelTest(percModel, X_train, Y_train)
+    print("感知机算法算法结果:{}".format(acc_perc))
+    
+    # 线性分类支持向量机
+    lin_svcModel = LinearSVC()
+    acc_lin_svc = ModelTest(lin_svcModel, X_train, Y_train)
+    print("线性分类支持向量机算法结果:{}".format(acc_lin_svc))
+    
+    # 梯度下降分类算法
+    sgdModel = SGDClassifier()
+    acc_sgd = ModelTest(sgdModel, X_train, Y_train)
+    print("梯度下降分类算法结果:{}".format(acc_sgd))
+    
+    # 决策树算法
+    treeModel = DecisionTreeClassifier()
+    acc_tree = ModelTest(treeModel, X_train, Y_train)
+    print("决策树算法结果:{}".format(acc_tree))
+    
+    # 随机森林算法
+    forestModel = RandomForestClassifier()
+    acc_rand = ModelTest(forestModel, X_train, Y_train)
+    print("随机森林算法结果:{}".format(acc_rand))
+    
+    # 模型评分
+    models = pd.DataFrame({
+    'Model': ['Support Vector Machines', 'KNN', 'Logistic Regression', 'Random Forest', 'Naive Bayes', 'Perceptron', 'Stochastic Gradient Decent', 'Linear SVC','Decision Tree'],
+    'Score': [acc_svc, acc_knn, acc_log, acc_rand, acc_bys, acc_perc, acc_sgd, acc_lin_svc, acc_tree]})
+    print(models.sort_values(by='Score', ascending=False))
+    
