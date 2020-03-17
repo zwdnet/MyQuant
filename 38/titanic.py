cd1493d55bd146d2ef327f185a98cef1c205ec42
zwdnet <zwdnet@163.com>
2020-03-17 09:05:36 +0800

    绘制模型学习曲线。

--- 38/titanic.py
+++ 38/titanic.py
@@ -15,10 +15,32 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
+from sklearn.learning_curve import learning_curve
+from sklearn.ensemble import RandomForestClassifier as RFC
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=1,train_sizes=np.linspace(.1, 1.0, 5),verbose=0):
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(estimator, X, y, cv=cv,n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,train_scores_mean + train_scores_std, alpha=0.1, color="r")
+    plt.fill_between(train_sizes,test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r", label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g", label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
 
 
-
-        
 if __name__ == "__main__":
     # 载入数据
     df_all = tools.load_data()
@@ -36,3 +58,12 @@ if __name__ == "__main__":
     
     # ④建模
     modeling.model(df_all)
+    
+    # ⑤模型评估
+    rf_parameters = {"criterion":"gini", "n_estimators":1750, "max_depth":7, "min_samples_split":6, "min_samples_leaf":6, "max_features":'auto', "oob_score":True, "random_state":SEED, "n_jobs":-1, "verbose":1}
+    title = "RandomForest"
+    df_train, df_test = tools.divide_df(df_all)
+    X_train = StandardScaler().fit_transform(df_train.drop(["Survived"], axis = 1))
+    y_train = df_train["Survived"].values
+    plt = plot_learning_curve(RFC(**rf_parameters), title, X_train, y_train, cv=None,  n_jobs=-1, train_sizes=[50, 100, 150, 200, 250, 350, 400, 450, 500])
+    plt.savefig("learningCurve.png")
