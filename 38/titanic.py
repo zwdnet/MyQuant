86ee68024600be3ae66865051938b1362f1ae030
zwdnet <zwdnet@163.com>
2020-03-17 01:16:55 +0000

    绘制学习曲线完毕，传回本地。

--- 38/titanic.py
+++ 38/titanic.py
@@ -15,8 +15,13 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-from sklearn.learning_curve import learning_curve
+from sklearn.model_selection import learning_curve
 from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.preprocessing import StandardScaler
+
+
+SEED = 42
+
 
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=1,train_sizes=np.linspace(.1, 1.0, 5),verbose=0):
     plt.figure()
