0d977a57b33c5400cd8438800b9dfe1bd1232fb7
zwdnet <zwdnet@163.com>
2020-06-29 15:12:41 +0800

    完成第19章图的深度优先搜索。

---
+++ 44/19/weekend.py
@@ -0,0 +1,38 @@
+# 《programming for the puzzled》实操
+# 19.周末聚会问题
+
+
+# 判断图是否为二分图
+def bipartiteGraphColor(graph, start, coloring, color):
+    if start not in graph:
+        return False, {}
+        
+    if start not in coloring:
+        coloring[start] = color
+    elif coloring[start] != color:
+        return False, {}
+    else:
+        return True, coloring
+        
+    if color == 'Sha':
+        newcolor = 'Hat'
+    else:
+        newcolor = 'Sha'
+        
+    for vertex in graph[start]:
+        val, coloring = bipartiteGraphColor(graph, vertex, coloring, newcolor)
+        if val == False:
+            return False, {}
+        
+    return True, coloring
+
+
+if __name__ == "__main__":
+    dangling = {"A":["B", "E"],
+                         "B":["A", "E", "C"],
+                         "C":["B", "D"],
+                         "D":["C", "E"],
+                         "E":["A", "B", "D"]}
+    res, col = bipartiteGraphColor(dangling, "A", {}, "Sha")
+    print(res, col)
+    
