a628eaf15e2fbdfd9ce6fcfff882edd5b4d85030
zwdnet <zwdnet@163.com>
2020-10-09 14:22:32 +0800

    完成分类算法的学习。

---
+++ 49/p3_rtf.py
@@ -0,0 +1,67 @@
+# coding:utf-8
+# 机器学习A-Z
+# 第三部分 分类算法 随机数森林
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import pandas as pd
+from sklearn.preprocessing import Imputer,LabelEncoder, OneHotEncoder, StandardScaler, PolynomialFeatures
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+
+
+if __name__ == "__main__":
+    dataset = pd.read_csv("./data/Machine Learning A-Z Chinese Template Folder/Part 3 - Classification/Section 15 - Random Forest Classification/Social_Network_Ads.csv")
+    print(dataset.info())
+    x = dataset.iloc[:, [2,3]].values
+    y = dataset.iloc[:, 4].values
+    # 特征缩放
+    sc_x = StandardScaler()
+    x = sc_x.fit_transform(x)
+    # 划分训练集和测试集
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
+    print(x_train[:10], y_test[:10]) 
+    # 训练分类器
+    classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0 )
+    classifier.fit(x_train, y_train)
+    # 用分类器预测测试集结果
+    y_pred = classifier.predict(x_test)
+    print(y_pred)
+    # 用混淆矩阵评估分类器    
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    # 画图显示分类结果
+    fig = plt.figure()
+    X_set, y_set = x_train, y_train
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('orange', 'blue'))(i), label = j, s = 15)
+    plt.title('Classifier (Training set)')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.savefig("train_rtf.png")
+
+    # Visualising the Test set results
+    fig = plt.figure()
+    X_set, y_set = x_test, y_test
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01), np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('orange', 'blue'))(i), label = j, s=15)
+    plt.title('Random Forest (Test set)')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.savefig("test_rtf.png")
+    
+    
